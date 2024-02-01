81c5909fadc1c623e3f090f9636f8bf5db2ddc31
Niladri2003 <niladri.adak.21@gmail.com>
2024-02-01 23:43:11 +0530

    Cors

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", "http://localhost:4000",
+      "http://localhost:3000",
+        "http://localhost:4000",
 
     ],
     credentials: true,
